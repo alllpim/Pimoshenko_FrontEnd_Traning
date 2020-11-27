93ec7ae1d855f1f4084093d2984d9792fc4f68ad
Alina Pimoshenko <pimoshenko63@gmail.com>
2020-11-27 12:17:17 +0300

    /

--- task1/js/dateTime.js
+++ task1/js/dateTime.js
@@ -1,13 +1,2 @@
-function DateTime() {
-  temp_date = new Date();
-  day = temp_date.getDate();
-  month = temp_date.getMonth() + 1;
-  year = temp_date.getYear();
-  if (day < 10) {
-  day = "0" + day;
-  }
-  if (month <10) {
-  month = "0" + month;
-  }
-  document.getElementById('footer').innerHTML = day + "." + month + "." + year;
-}
+var date = new Date();
+document.getElementById('datetime').innerHTML = date.toLocaleDateString();
--- task1/js/style.js
+++ task1/js/style.js
@@ -1,9 +1,3 @@
-function style1() {
-  document.getElementById("table").className = 'table';
-}
-function style2() {
-  document.getElementById("table").className = 'table2';
-}
-function style3() {
-  document.getElementById("table").className = 'table3';
+function styleNew(styleNew) {
+  document.getElementById('style').setAttribute('href', styleNew);
 }
